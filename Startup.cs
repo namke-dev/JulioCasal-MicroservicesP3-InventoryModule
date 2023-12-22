85251a8f1025e8c549c2012aef24e8cc4f6b57b6
namke-dev <namke.dev@gmail.com>
2023-12-23 01:35:44 +0700

    implement Regist RabbitMQ

--- Startup.cs
+++ Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using Play.Common.MassTransit;
 using Play.Common.MongoDb;
 using Play.Inventory.Service.Clients;
 using Play.Inventory.Service.Entities;
@@ -20,34 +21,17 @@ namespace Play.Inventory.Service
         {
             Configuration = configuration;
         }
-
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMongoDbServices()
-                        .AddMongoRepository<InventoryItem>("inventoryitems");
+                        .AddMongoRepository<InventoryItem>("inventoryitems")
+                        .AddMongoRepository<CatalogItem>("catalogitems")
+                        .AddMassTransitWithRabbitMq();
 
-            Random jitterer = new Random();
-            services.AddHttpClient<CatalogClient>(client =>
-            {
-                client.BaseAddress = new Uri("https://localhost:5001");
-            })
-            .AddTransientHttpErrorPolicy(builder => builder.WaitAndRetryAsync(
-                5,
-                retryAttemp => TimeSpan.FromSeconds(Math.Pow(2, retryAttemp))
-                            + TimeSpan.FromMilliseconds(jitterer.Next(0, 1000))
-
-            ))
-            .AddTransientHttpErrorPolicy(builder => builder.Or<TimeoutRejectedException>().CircuitBreakerAsync(
-                //If there are over 3 request at a time, the circuit will open
-                // => sequest sent will be fail immediately
-                3, TimeSpan.FromSeconds(15) //=> circuit will open for 15 secs
-            ))
-            .AddPolicyHandler(Policy.TimeoutAsync<HttpResponseMessage>(1));
-
-            services.AddControllers();
+            AddCatalogClient(services);
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Play.Inventory.Service", Version = "v1" });
@@ -74,6 +58,28 @@ namespace Play.Inventory.Service
             {
                 endpoints.MapControllers();
             });
+        }
+
+        private static void AddCatalogClient(IServiceCollection services)
+        {
+            Random jitterer = new Random();
+            services.AddHttpClient<CatalogClient>(client =>
+            {
+                client.BaseAddress = new Uri("https://localhost:5001");
+            })
+            .AddTransientHttpErrorPolicy(builder => builder.WaitAndRetryAsync(
+                5,
+                retryAttemp => TimeSpan.FromSeconds(Math.Pow(2, retryAttemp))
+                            + TimeSpan.FromMilliseconds(jitterer.Next(0, 1000))
+
+            ))
+            .AddTransientHttpErrorPolicy(builder => builder.Or<TimeoutRejectedException>().CircuitBreakerAsync(
+                //If there are over 3 request at a time, the circuit will open
+                // => sequest sent will be fail immediately
+                3, TimeSpan.FromSeconds(15) //=> circuit will open for 15 secs
+            ))
+            .AddPolicyHandler(Policy.TimeoutAsync<HttpResponseMessage>(1));
+            services.AddControllers();
         }
     }
 }
